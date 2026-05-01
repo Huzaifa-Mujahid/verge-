a652965e334d6c27a4fd4b28dab07bb0694d5a38
Huzaifa-Mujahid <huzaifa.official.dev@gmail.com>
2026-05-01 23:09:15 +0500

    Setup environment variables for Vercel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://bgzjcnbjrevsabtyvjgg.supabase.co'
-const supabaseAnonKey = 'sb_publishable_3nxbEt6Ez8yh86vqkZ1EKQ_1mOOrNeQ'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
